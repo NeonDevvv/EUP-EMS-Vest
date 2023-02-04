1761311b59c3d5f092c8fd4500268d77dc3bdf36
NeonDevvv <124428987+NeonDevvv@users.noreply.github.com>
2023-02-04 09:35:51 +0100

    Add files via upload

---
+++ emsvest-NeonDevvv/fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Generated with AltTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_swatvests.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_swatvests.meta'
